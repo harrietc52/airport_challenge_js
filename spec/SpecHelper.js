cdfbb58f7ee335daf9e7c8983cb6aca504af98a4
danielstpaul <danielstpaul@hotmail.com>
2015-09-28 16:42:31 +0100

    initial commit

---
+++ spec/SpecHelper.js
@@ -0,0 +1,15 @@
+beforeEach(function () {
+  jasmine.addMatchers({
+    toBePlaying: function () {
+      return {
+        compare: function (actual, expected) {
+          var plane = actual;
+
+          return {
+            pass: plane.currentlyPlayingSong === expected && plane.isPlaying
+          };
+        }
+      };
+    }
+  });
+});
